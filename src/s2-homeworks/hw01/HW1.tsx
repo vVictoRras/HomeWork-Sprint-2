c7dba20e5539bb7a211a91a013c9e13b9538441b
vVictorras <sirotilov@gmail.com>
2025-04-23 23:25:58 +0300

    add homepage in pacage.json

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -15,8 +15,8 @@ import message from "./message/Message";
 * */
 // нужно создать правильный тип вместо any
 export type MessageType = {
-  id: number,
-  user: userType;
+    id: number,
+    user: userType;
     message: newMessageType;
 }
 export type userType = {
@@ -59,12 +59,12 @@ const HW1 = () => {
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
+                    <Message message={message0}/>
+                    <FriendMessage message={friendMessage0}/>
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
+                <MessageSender M={Message}/>
             </div>
         </div>
     )
