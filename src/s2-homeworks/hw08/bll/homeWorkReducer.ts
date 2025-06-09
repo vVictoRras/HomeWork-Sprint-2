690d47e0d85729e33fd29c6d809fde0501f8fd3b
vVictorras <sirotilov@gmail.com>
2025-06-10 00:22:12 +0300

    hw8 is done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            const newState = [...state];
+            if (action.payload === 'up') {
+                return newState.sort((a, b) => a.name.localeCompare(b.name));
+            } else {
+                return newState.sort((a, b) => b.name.localeCompare(a.name));
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload);
         }
         default:
-            return state
+            return state;
     }
 }
